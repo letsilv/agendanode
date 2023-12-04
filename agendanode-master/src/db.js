633537cb477f6f8a3b78c3762424147aa5c2798a
letsilv <107430130+letsilv@users.noreply.github.com>
2023-12-04 19:41:12 -0300

    Update db.js

--- agendanode-master/src/db.js
+++ agendanode-master/src/db.js
@@ -1,3 +1,4 @@
+// importação do mysql2 para interagir com bancos de dados MySQL em ambientes Node.js.
 import mysql from 'mysql2';
 
 /* const connection = mysql.createConnection({
@@ -6,17 +7,17 @@ import mysql from 'mysql2';
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 }); */
-
+// configuração de conexão
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Admin@123',
     database: 'lojadb'
 });
-
+// conexão com o banco de dados
 connection.connect((error) => {
     if(error) throw error;
     console.log(`Conectado ao banco de dados: lojadb } `);
 });
-
+// exportação da conexão
 export default connection;
