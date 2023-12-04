c14fbaad32c7880a22e2f615496f3ad8aa664f1f
letsilv <107430130+letsilv@users.noreply.github.com>
2023-12-04 19:47:12 -0300

    Update routes.js

--- agendanode-master/src/routes.js
+++ agendanode-master/src/routes.js
@@ -1,13 +1,14 @@
+// importacção do express
 import express from 'express';
-
+// criação de um roteador
 const router = express.Router();
-
+// Importação das funções do controlador
 import { buscarTodos, buscarUm, inserir, alterar, excluir } from './controllers/ClienteController.js';
-
+// Definição das rotas
 router.get('/clientes', buscarTodos);
 router.get('/cliente/:Id_Cliente', buscarUm);
 router.post('/cliente', inserir);
 router.put('/cliente/:Id_Cliente', alterar);
 router.delete('/cliente/:Id_Cliente', excluir);
-
+// Exportação do roteador:
 export { router as default};
